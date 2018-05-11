045b827dd284cf2ac1b3a566d5d501cf50173e77
sarbelaezc <sarbelaezc@unal.edu.co>
2018-05-11 12:02:19 -0500

    plantilla para mostrar author details

--- app/routes/authors/author.js
+++ app/routes/authors/author.js
@@ -1,4 +1,13 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model(){
+    this.store.find('author', 'posts');
+  },
+  actions: {
+    delete(post){
+      console.log('llega');
+      this.posts.removeObject(post);
+    },
+  },
 });
